ce19acb678f1f282bc9a5939552779ede9fc8b72
Com1t <fuchiang137@gmail.com>
2020-02-26 21:51:25 +0800

    Inproved readability

--- TIME.ino
+++ TIME.ino
@@ -2,46 +2,48 @@
 #include <dht11.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd( 8, 7, 6, 5, 4, 3);
-int DHT11pin = 9;
+LiquidCrystal lcd( 8, 7, 6, 5, 4, 3);                  // RS ,En ,D4 ,D5 ,D6 ,D7
+/*LCD R/W pin to ground
+   A variable resistor ends to +5V and ground
+   wiper to LCD Vo pin (pin3)*/
+
 dht11 DHT11;
 
+const uint8_t DHT11pin = 9;
+
+const uint8_t DS1307_I2C_ADDRESS = 0x68;  // DS1307 or DS3231 (I2C) 地址
+const uint8_t NubberOfFields = 7;                   // DS1307 or DS3231 (I2C) 資料範圍
+uint16_t y;                                                   // 年
+uint8_t m, d, w, h, mi, s;                               // 月/日/週/時/分/秒
 char week[7][4] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
 char month[12][4] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
 
-const byte DS1307_I2C_ADDRESS = 0x68; // DS1307 (I2C) 地址
-const byte NubberOfFields = 7;        // DS1307 (I2C) 資料範圍
-
-int y;                                // 年
-byte m, d, w, h, mi, s;               // 月/日/週/時/分/秒
-
-
-// BCD 轉 DEC
-byte bcdTodec(byte val) {
+/*BCD 轉 DEC*/
+uint8_t bcdTodec(uint8_t val) {
   return ((val / 16 * 10) + (val % 16));
 }
 
+/*自DS1307取得時間*/
 void getTime() {
   Wire.beginTransmission(DS1307_I2C_ADDRESS);
   Wire.write(0);
   Wire.endTransmission();
-
   Wire.requestFrom(DS1307_I2C_ADDRESS, NubberOfFields);
 
-  s = bcdTodec(Wire.read() & 0x7f);
-  mi = bcdTodec(Wire.read());
-  h = bcdTodec(Wire.read() & 0x7f);
-  w = bcdTodec(Wire.read());
-  d = bcdTodec(Wire.read());
-  m = bcdTodec(Wire.read());
-  y = bcdTodec(Wire.read()) + 2000;
+  s    = bcdTodec(Wire.read() & 0x7f);
+  mi  = bcdTodec(Wire.read());
+  h    = bcdTodec(Wire.read() & 0x7f);
+  w   = bcdTodec(Wire.read());
+  d    = bcdTodec(Wire.read());
+  m   = bcdTodec(Wire.read());
+  y    = bcdTodec(Wire.read()) + 2000;
 }
 
-// 顯示時間
+/*顯示時間*/
 void digitalClockDisplay() {
-  byte me = m - 1;
+  uint8_t me = m - 1;
   if (me >= 0 && me <= 11) {
-    for (byte f = 0; f < 3; f++) {
+    for (uint8_t f = 0; f < 3; f++) {
       lcd.print(month[me][f]);
     }
   }
@@ -50,47 +52,61 @@ void digitalClockDisplay() {
   lcd.print(" ");
   lcd.print(y);
   lcd.setCursor(12, 0);
-  byte we = w - 1;
+  uint8_t we = w - 1;
   if (we >= 0 && we <= 6) {
-    for (byte e = 0; e < 3; e++) {
+    for (uint8_t e = 0; e < 3; e++) {
       lcd.print(week[we][e]);
     }
   }
   lcd.setCursor(4, 1);
+
+  if (h < 10) {
+    lcd.print("0");
+  }
   lcd.print(h);
-  lcd.print(":");
+  lcd.print(":");  
+  if (mi < 10) {
+    lcd.print("0");
+  }
   lcd.print(mi);
   lcd.print(":");
+  if (s < 10) {
+    lcd.print("0");
+  }
   lcd.print(s);
 }
 
 
 void setup() {
-  lcd.begin(16, 2);
 
   Wire.begin();
-  int i = 13;
-  lcd.clear(); // 設定游標位置在第一行行首
+
+  lcd.begin(16, 2);
+  lcd.clear();
   lcd.setCursor(2, 0);
   lcd.print("Now loading");
   delay(500);
-  while (i >= 13 && i <= 15) {
+
+  for (uint8_t i = 13; i >= 13 && i <= 15; i++) {
     lcd.setCursor(i, 0);
     lcd.print(".");
     delay(500);
-    i++;
   }
   delay(400);
+
   lcd.clear();
   lcd.print("READ SENSOR");
-  int chk = DHT11.read(DHT11pin);
-  lcd.setCursor(11, 1);
+  int8_t chk = DHT11.read(DHT11pin);
+
+
   switch (chk)
   {
     case 0:
-      lcd.print("   OK");
+      lcd.setCursor(14, 1);
+      lcd.print("OK");
       break;
     default:
+      lcd.setCursor(11, 1);
       lcd.print("ERROR");
       break;
   }
@@ -98,30 +114,33 @@ void setup() {
 }
 
 void loop() {
-  int chk = DHT11.read(9);
+  int8_t chk = DHT11.read(DHT11pin);
   boolean DHT11Botton = digitalRead(10);
 
   lcd.clear();
-  getTime(); // 取得時間
+  getTime();               // 取得時間
   digitalClockDisplay(); // 顯示時間
-  
-  if (DHT11Botton == HIGH) {
 
+  if (DHT11Botton == HIGH) {
     for (int positionCounter = 0; positionCounter <= 16; positionCounter++) {
       // scroll one position left:
       lcd.scrollDisplayLeft();
       // wait a bit:
       delay(200);
     }
+
     lcd.clear();
-    lcd.print("Temperature ");
+    lcd.print("Temperature");
+    lcd.setCursor(12, 0);
     lcd.print((int)DHT11.temperature);
     lcd.print((char)0xDF);
     lcd.print("C");
     lcd.setCursor(0, 1);
-    lcd.print("Humidity    ");
+    lcd.print("Humidity");
+    lcd.setCursor(12, 1);
     lcd.print((int)DHT11.humidity);
-    lcd.print(" %");
+    lcd.setCursor(15, 1);
+    lcd.print("%");
     delay(2000);
 
     for (int positionCounter = 0; positionCounter <= 16; positionCounter++) {
@@ -132,6 +151,5 @@ void loop() {
     }
     boolean DHT11Botton = LOW;
   }
-  
   delay(1000);
 }
